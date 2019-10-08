5dab661021c0818504eae5cc91f5bb6a4e2e3656
mlengse <anjangkn@gmail.com>
2019-10-08 11:24:37 +0700

    in

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { schedule } = require('node-cron')
 const runner = require('./runner')
-schedule('5 1 * * 0,6', async () => {
+// schedule('5 1 * * 0,6', async () => {
+;(async() => {
   for (let pusk of ['sibela', 'purwosari']) {
     console.log('start', pusk, new Date())
     await runner(pusk)
   }
-})
+})()
+// })
